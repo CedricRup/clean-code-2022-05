32eb3b4915c2a8a8bdc9b342e4b82b40aa6abbd3
Cédric Rup <cedric.rup@gmail.com>
2021-01-07 17:00:07 +0100

    Pourrification

--- java/Trivia/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ java/Trivia/src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -12,9 +12,9 @@ public class GameRunner {
 	public static void main(String[] args) {
 		Game aGame = new Game();
 		
-		aGame.add("Chet");
-		aGame.add("Pat");
-		aGame.add("Sue");
+		aGame.Add("Chet");
+		aGame.Add("Pat");
+		aGame.Add("Sue");
 		
 		Random rand = new Random();
 	
--- java/Trivia/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/Trivia/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -1,32 +1,52 @@
+///////////////////////////////////////////////
+///                                          //
+/// Jeu.cs                                   //
+///                                          //
+/// COpyright The TrivaGame Ltd              //
+///                                          //
+/// Change : 2000-08-17 : add Rock questions //
+/// Change : 2002-04-01: Formatting          //
+/// Bug 528491 : Fix penaltybox bug where player is stuck //
+///////////////////////////////////////////////
+
 package com.adaptionsoft.games.uglytrivia;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+
+/**
+ * The game class
+ */
 public class Game {
+
+	private final int FIVE = 6;
     ArrayList players = new ArrayList();
     int[] places = new int[6];
     int[] purses  = new int[6];
-    boolean[] inPenaltyBox  = new boolean[6];
+    boolean[] inPenaltyBox  = new boolean[FIVE];
     
-    LinkedList popQuestions = new LinkedList();
-    LinkedList scienceQuestions = new LinkedList();
-    LinkedList sportsQuestions = new LinkedList();
-    LinkedList rockQuestions = new LinkedList();
-    
+    LinkedList _Q1 = new LinkedList();
+    LinkedList Q2 = new LinkedList();
+    LinkedList<String> Q4 = new LinkedList<String>();
+
+
     int currentPlayer = 0;
     boolean isGettingOutOfPenaltyBox;
     
     public  Game(){
     	for (int i = 0; i < 50; i++) {
-			popQuestions.addLast("Pop Question " + i);
-			scienceQuestions.addLast(("Science Question " + i));
-			sportsQuestions.addLast(("Sports Question " + i));
-			rockQuestions.addLast(createRockQuestion(i));
+			_Q1.addLast("Pop Question " + i);
+			Q2.addLast(("Science Question " + i));
+			Q3.addLast(("Sports Question " + i));
+			Q6.addLast(createMovieQuestion(i));
     	}
+    	//shuf()
     }
 
-	public String createRockQuestion(int index){
+
+
+	public String createMovieQuestion(int index){
 		return "Rock Question " + index;
 	}
 	
@@ -34,7 +54,7 @@ public class Game {
 		return (howManyPlayers() >= 2);
 	}
 
-	public boolean add(String playerName) {
+	public boolean Add(String playerName) {
 		
 		
 	    players.add(playerName);
@@ -56,23 +76,26 @@ public class Game {
 		System.out.println("They have rolled a " + roll);
 		
 		if (inPenaltyBox[currentPlayer]) {
-			if (roll % 2 != 0) {
-				isGettingOutOfPenaltyBox = true;
-				
-				System.out.println(players.get(currentPlayer) + " is getting out of the penalty box");
-				places[currentPlayer] = places[currentPlayer] + roll;
-				if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
-				
-				System.out.println(players.get(currentPlayer) 
-						+ "'s new location is " 
-						+ places[currentPlayer]);
-				System.out.println("The category is " + currentCategory());
-				askQuestion();
-			} else {
+		if (roll % 2 != 0) {
+			//User is getting out of penalty box
+			isGettingOutOfPenaltyBox = true;
+			//Write tha user get out
+			System.out.println(players.get(currentPlayer) + " is getting out of the penalty box");
+			//Add roll to place
+			places[currentPlayer] = places[currentPlayer] + roll;
+			if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
+
+			System.out.println(players.get(currentPlayer)
+					+ "'s new location is "
+					+ places[currentPlayer]);
+			System.out.println("The category is " + currentCategory());
+			askQuestion();
+		}
+			else {
 				System.out.println(players.get(currentPlayer) + " is not getting out of the penalty box");
 				isGettingOutOfPenaltyBox = false;
 				}
-			
+
 		} else {
 		
 			places[currentPlayer] = places[currentPlayer] + roll;
@@ -87,17 +110,24 @@ public class Game {
 		
 	}
 
+	LinkedList Q6 = new LinkedList();
+
+
 	private void askQuestion() {
 		if (currentCategory() == "Pop")
-			System.out.println(popQuestions.removeFirst());
+			System.out.println(_Q1.removeFirst());
 		if (currentCategory() == "Science")
-			System.out.println(scienceQuestions.removeFirst());
+			System.out.println(Q2.removeFirst());
 		if (currentCategory() == "Sports")
-			System.out.println(sportsQuestions.removeFirst());
+			System.out.println(Q3.removeFirst());
 		if (currentCategory() == "Rock")
-			System.out.println(rockQuestions.removeFirst());		
+			System.out.println(Q6.removeFirst());
+		//shuf();
 	}
-	
+
+	private void shuf(){
+		Q4.stream().flatMap(q-> Q3.stream().map(o ->toString())).skip(5).sorted((a,b)-> (int) Math.abs(Math.sqrt((double)a) - (((int)b)^a.hashCode())));
+	}
 	
 	private String currentCategory() {
 		if (places[currentPlayer] == 0) return "Pop";
@@ -112,6 +142,9 @@ public class Game {
 		return "Rock";
 	}
 
+	/**
+	 * To Call when answer is right
+	 */
 	public boolean wasCorrectlyAnswered() {
 		if (inPenaltyBox[currentPlayer]){
 			if (isGettingOutOfPenaltyBox) {
@@ -151,7 +184,10 @@ public class Game {
 			return winner;
 		}
 	}
-	
+
+	/**
+	 * To Call when answer is right
+	 */
 	public boolean wrongAnswer(){
 		System.out.println("Question was incorrectly answered");
 		System.out.println(players.get(currentPlayer)+ " was sent to the penalty box");
@@ -159,6 +195,7 @@ public class Game {
 		
 		currentPlayer++;
 		if (currentPlayer == players.size()) currentPlayer = 0;
+		//Must alwys return false
 		return true;
 	}
 
@@ -166,4 +203,6 @@ public class Game {
 	private boolean didPlayerWin() {
 		return !(purses[currentPlayer] == 6);
 	}
+
+	LinkedList Q3 = new LinkedList();
 }
