a2aeef55a8c926984fcf3dde872cb2b17f7153ac
Cédric Rup <cedric.rup@gmail.com>
2021-01-07 17:14:20 +0100

    approvals

--- java/Trivia/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/Trivia/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -2,12 +2,102 @@ package com.adaptionsoft.games.trivia;
 
 import static org.junit.Assert.*;
 
+import com.adaptionsoft.games.uglytrivia.Game;
+import org.approvaltests.Approvals;
 import org.junit.Test;
+import sun.corba.OutputStreamFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.StringWriter;
 
 public class SomeTest {
 
 	@Test
-	public void true_is_true() throws Exception {
-		assertTrue(false);
+	public void Test1()
+	{
+		// Create a stream to hold the output
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream(baos);
+		PrintStream old = System.out;
+		System.setOut(ps);
+
+		Game game = new Game();
+		game.Add("Cedric");
+		game.roll(12);
+		game.wrongAnswer();
+		game.roll(2);
+		game.roll(13);
+		game.wasCorrectlyAnswered();
+		game.roll(13);
+		Approvals.verify(baos.toString());
+		System.setOut(old);
+
+	}
+
+        @Test
+	public void Test2()
+	{
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream(baos);
+		PrintStream old = System.out;
+		System.setOut(ps);
+		Game game = new Game();
+		game.Add("Cedric");
+		game.Add("Eloïse");
+		game.roll(1);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		Approvals.verify(baos.toString());
+		System.setOut(old);
+	}
+
+        @Test
+	public void Test3()
+	{
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream(baos);
+		PrintStream old = System.out;
+		System.setOut(ps);
+		Game game = new Game();
+		game.Add("Cedric");
+		game.Add("Eloïse");
+		game.roll(1);
+		game.wrongAnswer();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		game.roll(2);
+		game.wasCorrectlyAnswered();
+		Approvals.verify(baos.toString());
 	}
 }
